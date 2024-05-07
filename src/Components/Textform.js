2a3a55ea9ff1529d47b7c1f67b9e16eb1925590b
Shishir Kushwah <shishirkushwah@gmail.com>
2024-05-07 17:17:57 +0530

    Initial commit

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,10 +1,13 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
+import Translate from './Translate';
 
 export default function Textform(props) {
     const [text, setText] = useState('Enter URL');
+
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
+
     return (
         <div>   
             <h2>{props.heading}</h2>
@@ -16,15 +19,12 @@ export default function Textform(props) {
                     <button className="btn btn-primary">Get Summary</button>
                 </div>
             </div>
-  
-            <span style={{ margin: '0 10px' }}></span>
-            <h2>{props.heading1}</h2>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+            <div className="container my-3">
+                <h2>{props.heading1}</h2>
+                <Translate text={text} setText={setText} />
+
             </div>
+                
         </div>
     )
 }
-
-
-
