a20b8ebe934b30b1b9a45a4a37982bf807471ed2
Shishir Kushwah <shishirkushwah@gmail.com>
2024-05-10 14:15:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ function App() {
   return (
     <>
     <span style={{ margin: '0 10px' }}></span>
-    {/* <div className="container">
-      <h2>Enter your chatgpt key</h2>
-        <textarea className="form-control" value={"Enter your key"} id="myBox" rows="1"></textarea>
-    </div> */}
-    <span style={{ margin: '0 10px' }}></span>
     <div className="container" my3="true">
       <Textform heading="Enter the URL" heading1="Your Summary"></Textform>
     </div>
